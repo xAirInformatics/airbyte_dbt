55f7f9acd924ccca65ea8ce7f7dab8894b62ce69
Ziad Mansor <ziad@gmail.com>
2023-10-28 15:36:38 +0300

    refactor modules

---
+++ models/generated/airbyte_tables/public/fsr.sql
@@ -0,0 +1,7 @@
+{{ config(schema="raw_data") }}
+-- Final base SQL model
+select
+    file_name
+from {{ ref('poke_pokemon_abilities_ability_ab3') }}
+-- ability at poke_pokemon/abilities/ability from {{ ref('poke_pokemon_abilities') }}
+
